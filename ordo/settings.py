989f573654fdb549e7667bd254e30ea10d14e4ee
Nazar Mambetov <voldezar@gmail.com>
2016-12-12 14:59:18 +0300

    Add 1 room flats for sale links and titles

--- ordo/settings.py
+++ ordo/settings.py
@@ -13,7 +13,7 @@ BOT_NAME = 'ordo'
 
 SPIDER_MODULES = ['ordo.spiders']
 NEWSPIDER_MODULE = 'ordo.spiders'
-
+FEED_EXPORT_ENCODING = 'utf-8'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'ordo (+http://www.yourdomain.com)'
--- ordo/spiders/titles_spider.py
+++ ordo/spiders/titles_spider.py
@@ -1,17 +1,15 @@
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "links_sell_1k"
+class FlatsSpider(scrapy.Spider):
+    name = "flats"
     start_urls = [
 	'http://diesel.elcat.kg/lofiversion/index.php?f459-0.html'
     ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-
-            response.xpath('//div[@id="ipbcontent"]//a/text()').extract()
-            response.xpath('//div[@id="ipbcontent"]//a/@href').extract()
+        for flat in response.css('div.topicwrap li'):
+            yield {
+                'title': flat.css('a::text').extract_first(),
+                'link': flat.css('a::attr(href)').extract_first(),
+            }
