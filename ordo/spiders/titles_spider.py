b7632ac1d3e85e16c8a14646955634025b2334ee
Nazar Mambetov <voldezar@gmail.com>
2016-12-13 11:28:39 +0300

    Add parse method for pages

--- ordo/spiders/titles_spider.py
+++ ordo/spiders/titles_spider.py
@@ -4,11 +4,17 @@ import scrapy
 class FlatsSpider(scrapy.Spider):
     name = "flats"
     start_urls = [
-	'http://diesel.elcat.kg/lofiversion/index.php?f459-0.html'
+	'http://diesel.elcat.kg/lofiversion/index.php?f459.html'
     ]
 
     def parse(self, response):
-        for flat in response.css('div.topicwrap li'):
+        pages = response.css('div.ipbpagespan a::attr(href)').extract()
+        for page in pages:
+            yield scrapy.Request(page, callback=self.parse_page)
+            
+    def parse_page(self, response):
+        flats = response.css('div.topicwrap li')
+        for flat in flats:
             yield {
                 'title': flat.css('a::text').extract_first(),
                 'link': flat.css('a::attr(href)').extract_first(),
