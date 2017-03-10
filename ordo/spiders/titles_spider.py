1ba86390f97ccb5d64f7291ba944b1082fd0c438
Nazar Mambetov <voldezar@gmail.com>
2017-03-10 17:26:44 +0300

    change css to xpath

--- ordo/spiders/titles_spider.py
+++ ordo/spiders/titles_spider.py
@@ -3,20 +3,23 @@ import scrapy
 
 class FlatsSpider(scrapy.Spider):
     name = "flats"
-    start_urls = [
-	'http://diesel.elcat.kg/lofiversion/index.php?f459.html'
-    ]
+    start_urls = ["http://diesel.elcat.kg/index.php?showforum=459"]
 
     def parse(self, response):
-        pages = response.css('div.ipbpagespan a::attr(href)').extract()
+        pages = response.xpath('//li[@class="next"]/a/@href').extract_first()
         for page in pages:
             yield scrapy.Request(page, callback=self.parse_page)
             
     def parse_page(self, response):
-        flats = response.css('div.topicwrap li')
+        flats = response.xpath('//td[@class="col_f_content "]')
         for flat in flats:
             yield {
-                'title': flat.css('a::text').extract_first(),
-                'link': flat.css('a::attr(href)').extract_first(),
-                'price': flat.css('a::text').re('\d+\$'),
+                'title': flat.xpath('h4/a/span/text()').extract_first(),
+                'link': flat.xpath('h4/a/@href').extract_first(),
+#                'price': flat.xpath('h4/a/span/text()').re('\d{3-7}\$?'),
+#                'description': scrapy.Request(flat, callback=self.parse_description)
             }
+
+#    def parse_description(self, response):
+#        description = response.css('div.postcontent::text').extract_first()
+
