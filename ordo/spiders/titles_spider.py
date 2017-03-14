4ed419190701c141a2c9f46d641f94833ad979c1
Nazar Mambetov <voldezar@gmail.com>
2017-03-14 17:08:21 +0300

    fix following links

--- ordo/spiders/titles_spider.py
+++ ordo/spiders/titles_spider.py
@@ -3,23 +3,18 @@ import scrapy
 
 class FlatsSpider(scrapy.Spider):
     name = "flats"
-    start_urls = ["http://diesel.elcat.kg/index.php?showforum=459"]
+    start_urls = ['http://diesel.elcat.kg/index.php?showforum=459']
 
     def parse(self, response):
-        pages = response.xpath('//li[@class="next"]/a/@href').extract_first()
-        for page in pages:
-            yield scrapy.Request(page, callback=self.parse_page)
-            
-    def parse_page(self, response):
         flats = response.xpath('//td[@class="col_f_content "]')
         for flat in flats:
             yield {
                 'title': flat.xpath('h4/a/span/text()').extract_first(),
                 'link': flat.xpath('h4/a/@href').extract_first(),
-#                'price': flat.xpath('h4/a/span/text()').re('\d{3-7}\$?'),
-#                'description': scrapy.Request(flat, callback=self.parse_description)
+                'price': flat.xpath('h4/a/span/text()').re('\d{3-7}\$?'),
             }
 
-#    def parse_description(self, response):
-#        description = response.css('div.postcontent::text').extract_first()
-
+        next_page = response.xpath('//li[@class="next"]/a/@href').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
